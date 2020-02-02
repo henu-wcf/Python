5b7a55fb020cb50d8848990d633485b7b7beb676
henu123 <48373564+henu123@users.noreply.github.com>
2020-02-02 23:30:38 +0800

    算法：递归 后序遍历二叉树

---
+++ 计蒜客A1004copy2.py
@@ -0,0 +1,35 @@
+# 后序遍历二叉树
+# 算法：递归遍历 houXu(root, kv)
+
+# 注意 左右根：左右是指左子树和右子树，
+# 对左子树再进行左右根直至结束，然后对右子树进行左右根直至结束
+# 递归容易迷，可以进行断点调试
+def main():
+    result = []
+    kv = {}
+    line = input().split(" ")
+    for li in line:
+        newline = li.split("(")
+        key = newline[1][0:3]
+        value = newline[0]
+        kv[key] = value
+
+    print(kv)
+    root = "0,0"
+    houXu(root, kv)
+def houXu(root, kv):
+
+    if root in kv:
+        #左
+        lchild = kv[root]+",1" #前一个根节点的左子节点
+        houXu(lchild, kv)
+        #右
+        rchild = kv[root]+',2'#前一个根节点的右子节点
+        houXu(rchild, kv)
+        #根
+        print(kv[root])
+
+    else :
+        return     #返回到上一层调用处
+
+main()
